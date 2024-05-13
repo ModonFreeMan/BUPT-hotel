f8102edcd34347d13c66a338414ec2646fe171ec
陈俞涵 <xianglai2003@icloud.com>
2024-05-13 19:24:45 +0800

    跨域问题解决

--- backend/src/main/java/com/backend/config/GlobalConfiguration.java
+++ backend/src/main/java/com/backend/config/GlobalConfiguration.java
@@ -14,6 +14,10 @@ import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 @Configuration
 public class GlobalConfiguration {
 
@@ -64,6 +68,22 @@ public class GlobalConfiguration {
     public CopyOnWriteArrayList<String> initRecoveryQueue() {
         return new CopyOnWriteArrayList<>();
     }
+
+    // 当前跨域请求最大有效时长。这里默认1天
+    private static final long MAX_AGE = 24 * 60 * 60;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 1 设置访问源地址
+        corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
+        corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法
+        corsConfiguration.setMaxAge(MAX_AGE);
+        source.registerCorsConfiguration("/**", corsConfiguration); // 4 对接口配置跨域设置
+        return new CorsFilter(source);
+    }
+
 
 
 }
